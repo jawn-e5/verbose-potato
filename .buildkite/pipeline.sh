938473ed778d52ca10f5f9a2baf12f1264cadad4
Tyler Auerbeck <tylerauerbeck@outlook.com>
2021-12-25 04:13:26 +0000

    test

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,12 +4,21 @@
 set -eu
 
 # begin the pipeline.yml file
-echo "steps:"
+echo "\
+steps:
+  - input: Choose A Repository
+    fields:
+      - text: Repository
+        key: repo
+	multiple: true
+	options:	
+"
 
 # add a new command step to run the tests in each test directory
 repos=$(curl -s -u $GH_USER:$GH_TOKEN https://api.github.com/orgs/highvolteej/repos | jq '.[].name')
 
 for repo in $repos; do
   repo=$( echo $repo | sed 's/\"//g')
-  echo "  - command: \"echo "${repo}"\""
+  echo "          - label: ${repo}"
+  echo "          - key: ${repo}"
 done
