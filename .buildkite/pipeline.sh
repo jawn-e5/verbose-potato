bd6a5c9d30c04086399e6074cce8fd33d73fb3fe
Tyler Auerbeck <tylerauerbeck@outlook.com>
2021-12-25 05:13:52 +0000

    weee

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -6,15 +6,15 @@ set -eu
 # begin the pipeline.yml file
 echo "steps:"
 echo "  - input: Choose An Organization"
-echo "    key: org
-          fields:
-	    - select: org
-	      key: orgcheck
-	      options:
-	        - label: jawn-e5
-		  value: jawn-e5
-		- label: auerbecklabs
-		  value: auerbecklabs"
+echo "    key: org"
+echo "     fields:"
+echo "	     - select: org"
+echo "	       key: orgcheck"
+echo "	       options:"
+echo "	         - label: jawn-e5"
+echo "             value: jawn-e5"
+echo "		 - label: auerbecklabs"
+echo "		   value: auerbecklabs"
 echo "  - wait"
 echo "  - input: Choose A Repository"
 echo "    key: 'check'"
