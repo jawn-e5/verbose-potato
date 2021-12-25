0b418db7baa3c45bcef7c738f5d5efc492d8fc93
Tyler Auerbeck <tylerauerbeck@outlook.com>
2021-12-25 23:26:12 +0000

    burn it down

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,16 +5,19 @@ set -eu
 
 # begin the pipeline.yml file
 echo "steps:"
-echo "  - input: Choose An Organization"
-echo "    key: org"
-echo "    fields:"
-echo "	    - select: 'org'"
-echo "	      key: 'orgcheck'"
-echo "	      options:"
-echo "	        - label: 'jawn-e5'"
-echo "            value: 'jawn-e5'"
-echo "          - label: 'auerbecklabs'"
-echo "            value: 'auerbecklabs'"
+echo "  - input: "Release details"
+          fields:
+            - select: "Stream"
+              key: "release-stream"
+              hint: "Which release stream does this belong in? :fork:"
+              required: false
+              default: "beta"
+              options:
+                - label: "Beta"
+                  value: "beta"
+                - label: "Stable"
+                 value: "stable"
+"
 echo "  - wait"
 echo "  - input: Choose A Repository"
 echo "    key: 'check'"
