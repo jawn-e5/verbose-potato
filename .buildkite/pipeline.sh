ba8a066b441feb94bdc23284f273a29e38a265f0
Tyler Auerbeck <tylerauerbeck@outlook.com>
2021-12-25 04:06:11 +0000

    tester

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -8,7 +8,8 @@ echo "steps:"
 
 # add a new command step to run the tests in each test directory
 repos=$(curl -s -u $GH_USER:$GH_TOKEN https://api.github.com/orgs/highvolteej/repos | jq '.[].name')
-for repo in repos; do
-  echo "  - command: \"echo ${repo}\""
+
+for repo in $repos; do
+  repo=$( echo $repo | sed 's/\"//g')
   echo "  - command: \"echo "${repo}"\""
 done
