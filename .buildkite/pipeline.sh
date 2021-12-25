28b22050c5ec4f4f7147fdb9667d9c859fa958a5
jawn-e5 <94623252+jawn-e5@users.noreply.github.com>
2021-12-24 22:44:03 -0500

    Create pipeline.sh
    asdf

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+# begin the pipeline.yml file
+echo "steps:"
+
+# add a new command step to run the tests in each test directory
+repos=$(curl -s -u \\$GH_USER:\\$GH_TOKEN https://api.github.com/orgs/highvolteej/repos | jq '.[].name')
+for repo in repos; do
+  echo "  - command: \"echo ${repo}"\""
+done
