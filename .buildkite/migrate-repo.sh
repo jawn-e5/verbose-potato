37d2649590439a2420854ce2d627226f190141b2
Tyler Auerbeck <tylerauerbeck@outlook.com>
2021-12-26 08:35:50 +0000

    fix

--- .buildkite/migrate-repo.sh
+++ .buildkite/migrate-repo.sh
@@ -7,5 +7,5 @@ DEST=$(buildkite-agent meta-data get destorg)
 
 
 for i in $RC; do
-	curl -s -u $GH_USER:GH_TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${SRC}/${i}/transfer -d "{\"new_owner\":\"${DEST}\"}"		
+	curl -s -u $GH_USER:$GH_TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${SRC}/${i}/transfer -d "{\"new_owner\":\"${DEST}\"}"		
 done
