8b740e1cfa08d5a5a145120fa45fa0c897153b84
Tyler Auerbeck <tylerauerbeck@outlook.com>
2021-12-26 08:34:24 +0000

    test

--- .buildkite/migrate-repo.sh
+++ .buildkite/migrate-repo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 RC=$(buildkite-agent meta-data get repocheck)
+SRC=$(buildkite-agent meta-data get srcorg)
+DEST=$(buildkite-agent meta-data get destorg)
+
 
 
 for i in $RC; do
-	echo "Repo: ${i}"
+	curl -s -u $GH_USER:GH_TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${SRC}/${i}/transfer -d "{\"new_owner\":\"${DEST}\"}"		
 done
